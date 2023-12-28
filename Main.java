4649cb8d572d52335fae16ef28e79a91c3e36f02
mharada337 <mharada27.amherst.edu>
2023-12-28 10:25:49 +0900

    new method

--- Main.java
+++ Main.java
@@ -1,39 +1,34 @@
 public class Main {
-    public static void main(String[] args) {
-        int[][] ground = new int[9][9];
-        for(int flags = 0; flags < 10; flags++) {
-            int x = (int)(Math.random() * 9);
-            int y = (int)(Math.random() * 9);
-            ground[x][y] = 9;
-        }
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
-                //test
-                // if((i == 0) && (ground[i][j] == 9)) {
-                //     ground[i][j - 1]++;
-                //     ground[i][j + 1]++;
-                // }
 
-                // if(ground[i][j] == 9) {
-                //     if((i - 1 >= 0) && (j != 8) && (j != 0)) {
-                //         ground[i - 1][j - 1]++;
-                //         ground[i - 1][j]++;
-                //         ground[i - 1][j + 1]++;
-                //     }
-                //     if((j != 8) && (j != 0)) {
-                //         ground[i][j - 1]++;
-                //         ground[i][j + 1]++;
-                //     }
-                //     if((i + 1 < 9) && (j != 8) && (j != 0)) {
-                //         ground[i + 1][j - 1]++;
-                //         ground[i + 1][j]++;
-                //         ground[i + 1][j + 1]++;
-                //     }
-                // }
+    public static int[][] ground = new int[10][10];
+
+    public static void main(String[] args) {
+
+        setMines(ground);
+        setMarkers(ground);
+
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < 10; j++) {
                 System.out.print(ground[i][j] + " ");
             }
             System.out.println();
         }
 
     }
+
+    public static void setMines(int[][] ground) {
+
+        for(int mines = 0; mines < 10; mines++) {
+            int x = (int)(Math.random() * 8) + 1;
+            int y = (int)(Math.random() * 8) + 1;
+            ground[x][y] = 9;
+        }
+
+    }
+
+    public static void setMarkers(int[][] ground) {
+        for(int i = 0; i < 10; i++) {
+        }
+    }
+
 }
